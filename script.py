2a1646103d7c89f559f0c69fd49ef6e9eca894ce
maisel <maikoltondini@gmail.com>
2025-02-24 23:17:14 +0100

    Added spaceship configurator

--- script.py
+++ script.py
@@ -26,15 +26,13 @@ class Player:
               print("Your input is not valid. Type 1 for oxygen, 2 for ammonia.")
           except ValueError:
             print("Invalid input. Please enter a number.")
-
-    #da implementare un po' di logica... domani - valutare rimozione del blocco else
     
 
 #Spaceship        
 class Spaceship:
     def __init__(self, name):
         self.name = name
-        self.storage = 3
+        self.storage = 0
         self.fuel = 0
         self.equipment = 0
         self.has_storage = True
@@ -42,6 +40,44 @@ class Spaceship:
     
     def __repr__(self):
         return f"Welcome on board! You are on the {self.name} spaceship."
+      
+    def add_fuel(self, amount):
+        if self.storage + amount <= 3:
+            self.fuel += amount
+            self.storage += amount
+            print(f"Added {amount} units of fuel. Total fuel: {self.fuel}/3")
+        else:
+            print("You're out of storage")
+
+    def add_equipment(self, amount):
+        if self.storage + amount <= 3:
+            self.equipment += amount
+            self.storage += amount
+            print(f"Added {amount} units of equipment. Total equipment: {self.equipment}")
+        else:
+            print("You're out of storage")
+
+#Spaceship configuration
+def spaceship_configuration(spaceship):
+    while True:
+        choice = input("Do you want to add fuel (f) or equipment (e) to the storage? (q to exit): ")
+        if choice.lower() == 'f':
+            try:
+                amount = int(input("How many units of fuel do you want to add? "))
+                spaceship.add_fuel(amount)
+            except ValueError:
+                print("Input is not valid. Please type a number.")
+        elif choice.lower() == 'e':
+            try:
+                amount = int(input("How many unit of equipment do you want to add? "))
+                spaceship.add_equipment(amount)
+            except ValueError:
+                print("Input is not valid. Please type a number.")
+        elif choice.lower() == 'q':
+            break
+        else:
+            print("Scelta non valida.")
+      
 
 #Planet
 class Planet:
@@ -67,13 +103,13 @@ print(introduction)
 player_name = input("""First of all, introduce yourself.
 what is your name?
 """)
-
 main_player = Player(player_name, "")
 main_player.define_breathing()
 print(main_player)
 
 #defining spaceship
-test_spaceship = Spaceship("Test")
+test_spaceship = Spaceship("Zero Gravity")
+spaceship_configuration(test_spaceship)
 print(test_spaceship)
 
 #defining planet
